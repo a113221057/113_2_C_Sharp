1ccbbed0ddfc5d5f4e927dc45ce275d56a23ac3c
a113221057 <a113221057@mail.shu.edu.tw>
2025-05-15 13:00:03 +0800

    114-05-15

---
+++ 114-05-15/CSV Reader/CSV Reader/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CSV_Reader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void getScoresButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                StreamReader inputFile;
+                string line;// 儲存讀取的每一行資料
+                int count = 0;// 計算行數
+                int total = 0;// 計算總分
+                double average = 0;// 計算平均分數
+
+
+                char[]delim = {',',':'};
+                // 顯示檔案選擇對話方塊，讓使用者選擇要開啟的 CSV 檔案
+                if (openFile.ShowDialog() == DialogResult.OK)
+                {
+                    // 開啟使用者選擇的檔案進行讀取
+                    inputFile = File.OpenText(openFile.FileName);
+                   
+                    while (!inputFile.EndOfStream)
+                    {
+                        // 讀取每一行資料
+                        line = inputFile.ReadLine();
+                        line = line.Trim(); // 去除行首行尾的空白字元
+                        string[] tokens= line.Split(delim); 
+                        total = 0; // 每次讀取新行時，重置總分為 0
+                        for (int i = 1; i < tokens.Length; i++)
+                        {
+                            // 將字串轉換為整數並累加到總分中
+                            total +=  int.Parse(tokens[i]);
+                        }
+                        average = (double)total /( tokens.Length -1);
+                        averagesListBox.Items.Add("第" + (++count) + "的平均分數為：" + average.ToString("F2"));
+                    }
+                }
+                else
+                {
+                    // 若使用者未選擇檔案，顯示提示訊息
+                    MessageBox.Show("未選擇任何檔案。");
+                }
+            }
+            catch (Exception ex)
+            {
+                // 發生例外狀況時，顯示錯誤訊息與詳細內容
+                MessageBox.Show("發生錯誤：" + ex.Message);
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // 關閉視窗並結束程式
+            this.Close();
+        }
+    }
+}
---
+++ 114-05-15/Structure Argument/Structure Argument/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Structure_Argument
+{
+    // 定義一個汽車（Automobile）結構，包含廠牌、年份與里程數欄位
+    struct Automobile
+    {
+        public string make;    // 汽車廠牌
+        public int year;       // 汽車年份
+        public double mileage; // 汽車里程數
+    }
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // DisplayAuto 方法接收一個 Automobile 結構物件作為參數，
+        // 並顯示該物件的所有欄位資訊於訊息方塊中
+        private void DisplayAuto(Automobile auto)
+        {
+            MessageBox.Show(
+                auto.year + " 年 " + auto.make +
+                "，里程數：" + auto.mileage + " 英里。"
+            );
+        }
+
+        // 當使用者點擊 auto1Button（顯示汽車 #1 按鈕）時執行此事件處理方法
+        private void auto1Button_Click(object sender, EventArgs e)
+        {
+            // 建立一個 Automobile 結構的實例，代表一台跑車
+            Automobile sportsCar = new Automobile();
+
+            // 指定該跑車的廠牌、年份與里程數
+            sportsCar.make = "Chevy Corvette";
+            sportsCar.year = 1970;
+            sportsCar.mileage = 50000.0;
+
+            // 顯示該跑車的所有資訊
+            DisplayAuto(sportsCar);
+        }
+
+        // 當使用者點擊 auto2Button（顯示汽車 #2 按鈕）時執行此事件處理方法
+        private void auto2Button_Click(object sender, EventArgs e)
+        {
+            // 建立一個 Automobile 結構的實例，代表一台皮卡車
+            Automobile pickupTruck = new Automobile();
+
+            // 指定該皮卡車的廠牌、年份與里程數
+            pickupTruck.make = "Ford Ranger";
+            pickupTruck.year = 1985;
+            pickupTruck.mileage = 75000.0;
+
+            // 顯示該皮卡車的所有資訊
+            DisplayAuto(pickupTruck);
+        }
+
+        // 當使用者點擊 auto3Button（顯示汽車 #3 按鈕）時執行此事件處理方法
+        private void auto3Button_Click(object sender, EventArgs e)
+        {
+            // 建立一個 Automobile 結構的實例，代表一台轎車
+            Automobile sedan = new Automobile();
+
+            // 指定該轎車的廠牌、年份與里程數
+            sedan.make = "Honda Accord";
+            sedan.year = 2000;
+            sedan.mileage = 80000.0;
+
+            // 顯示該轎車的所有資訊
+            DisplayAuto(sedan);
+        }
+    }
+}
