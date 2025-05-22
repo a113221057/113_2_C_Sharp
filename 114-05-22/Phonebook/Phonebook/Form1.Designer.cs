6fd3042483545bb6a77b9d550c434ed531a871d7
a113221057 <a113221057@mail.shu.edu.tw>
2025-05-22 12:54:06 +0800

    114-05-22

---
+++ 114-05-22/Phonebook/Phonebook/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace Phonebook
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.promptLabel = new System.Windows.Forms.Label();
+            this.nameListBox = new System.Windows.Forms.ListBox();
+            this.selectedPhoneDescriptionLabel = new System.Windows.Forms.Label();
+            this.phoneLabel = new System.Windows.Forms.Label();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.通訊錄 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.backgroundWorker3 = new System.ComponentModel.BackgroundWorker();
+            this.button = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.通訊錄.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // promptLabel
+            // 
+            this.promptLabel.AutoSize = true;
+            this.promptLabel.Location = new System.Drawing.Point(44, 11);
+            this.promptLabel.Name = "promptLabel";
+            this.promptLabel.Size = new System.Drawing.Size(65, 12);
+            this.promptLabel.TabIndex = 0;
+            this.promptLabel.Text = "請選擇姓名";
+            // 
+            // nameListBox
+            // 
+            this.nameListBox.FormattingEnabled = true;
+            this.nameListBox.ItemHeight = 12;
+            this.nameListBox.Location = new System.Drawing.Point(22, 26);
+            this.nameListBox.Name = "nameListBox";
+            this.nameListBox.Size = new System.Drawing.Size(120, 88);
+            this.nameListBox.TabIndex = 1;
+            this.nameListBox.SelectedIndexChanged += new System.EventHandler(this.nameListBox_SelectedIndexChanged);
+            // 
+            // selectedPhoneDescriptionLabel
+            // 
+            this.selectedPhoneDescriptionLabel.AutoSize = true;
+            this.selectedPhoneDescriptionLabel.Location = new System.Drawing.Point(173, 41);
+            this.selectedPhoneDescriptionLabel.Name = "selectedPhoneDescriptionLabel";
+            this.selectedPhoneDescriptionLabel.Size = new System.Drawing.Size(53, 12);
+            this.selectedPhoneDescriptionLabel.TabIndex = 2;
+            this.selectedPhoneDescriptionLabel.Text = "電話號碼";
+            // 
+            // phoneLabel
+            // 
+            this.phoneLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.phoneLabel.Location = new System.Drawing.Point(162, 66);
+            this.phoneLabel.Name = "phoneLabel";
+            this.phoneLabel.Size = new System.Drawing.Size(100, 21);
+            this.phoneLabel.TabIndex = 3;
+            this.phoneLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(105, 141);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 21);
+            this.exitButton.TabIndex = 4;
+            this.exitButton.Text = "離開";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // 通訊錄
+            // 
+            this.通訊錄.Controls.Add(this.textBox2);
+            this.通訊錄.Controls.Add(this.textBox1);
+            this.通訊錄.Controls.Add(this.button);
+            this.通訊錄.Controls.Add(this.label2);
+            this.通訊錄.Controls.Add(this.label1);
+            this.通訊錄.Location = new System.Drawing.Point(371, 12);
+            this.通訊錄.Name = "通訊錄";
+            this.通訊錄.Size = new System.Drawing.Size(200, 129);
+            this.通訊錄.TabIndex = 5;
+            this.通訊錄.TabStop = false;
+            this.通訊錄.Text = "通訊錄";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "姓名";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "電話";
+            // 
+            // backgroundWorker3
+            // 
+            this.backgroundWorker3.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker3_DoWork);
+            // 
+            // button
+            // 
+            this.button.Location = new System.Drawing.Point(84, 100);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(75, 23);
+            this.button.TabIndex = 2;
+            this.button.Text = "加入";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(84, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(84, 60);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(610, 174);
+            this.Controls.Add(this.通訊錄);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.phoneLabel);
+            this.Controls.Add(this.selectedPhoneDescriptionLabel);
+            this.Controls.Add(this.nameListBox);
+            this.Controls.Add(this.promptLabel);
+            this.Name = "Form1";
+            this.Text = "電話簿";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.通訊錄.ResumeLayout(false);
+            this.通訊錄.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label promptLabel;
+        private System.Windows.Forms.ListBox nameListBox;
+        private System.Windows.Forms.Label selectedPhoneDescriptionLabel;
+        private System.Windows.Forms.Label phoneLabel;
+        private System.Windows.Forms.Button exitButton;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+        private System.Windows.Forms.GroupBox 通訊錄;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button;
+    }
+}
+
---
+++ 114-05-22/Phonebook/Phonebook/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Phonebook
+{
+    // PhoneBookEntry 結構用來儲存每一筆電話簿資料，包括姓名與電話號碼
+    struct PhoneBookEntry
+    {
+        public string name;   // 姓名
+        public string phone;  // 電話號碼
+    }
+
+    public partial class Form1 : Form
+    {
+        // 用來儲存所有電話簿資料的 List
+        private List<PhoneBookEntry> phoneList =
+            new List<PhoneBookEntry>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // ReadFile 方法會讀取 PhoneList.txt 檔案內容，
+        // 並將每一筆資料轉換成 PhoneBookEntry 物件後存入 phoneList。
+        // 檔案每一行格式為：姓名,電話號碼
+        private void ReadFile()
+        {
+            try
+            {
+                // 檢查檔案是否存在
+                if (File.Exists("PhoneList.txt"))
+                {
+                    // 讀取檔案的每一行
+                    string[] lines = File.ReadAllLines("PhoneList.txt");
+
+                    // 逐行處理
+                    foreach (string line in lines)
+                    {
+                        // 分割姓名與電話號碼
+                        string[] parts = line.Split(',');
+
+                        // 確保格式正確
+                        if (parts.Length == 2)
+                        {
+                            PhoneBookEntry entry;
+                            entry.name = parts[0].Trim();
+                            entry.phone = parts[1].Trim();
+
+                            // 加入到 phoneList
+                            phoneList.Add(entry);
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("PhoneList.txt 檔案不存在。", "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"讀取檔案時發生錯誤: {ex.Message}", "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // DisplayNames 方法會將 phoneList 中所有姓名顯示在 nameListBox 控制項中，
+        // 讓使用者可以從清單中選擇要查詢的姓名。
+        private void DisplayNames()
+        {
+            // 清空 nameListBox 的項目
+            nameListBox.Items.Clear();
+
+            // 將 phoneList 中的每個姓名加入到 nameListBox
+            foreach (var entry in phoneList)
+            {
+                nameListBox.Items.Add(entry.name);
+            }
+        }
+
+        // Form1_Load 事件在表單載入時觸發，
+        // 主要負責初始化資料，例如讀取電話簿檔案並顯示姓名清單。
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ReadFile();
+            DisplayNames();
+            DisplayNames();
+        }
+
+        // nameListBox_SelectedIndexChanged 事件在使用者選取不同姓名時觸發，
+        // 會根據選取的姓名顯示對應的電話號碼於 phoneLabel。
+        private void nameListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int index = nameListBox.SelectedIndex;
+            if ((index != -1))
+            {
+                phoneLabel.Text = phoneList[index].phone;
+            }
+            else
+            {
+                phoneLabel.Text = "無資料";
+            }
+        }
+
+        // 按下離開按鈕時，會檢查是否有開啟過檔案。
+        // 若有開啟過檔案，則顯示確認訊息，提醒使用者未儲存的更改將會遺失。
+        // 若使用者選擇「是」，則關閉表單；否則不執行任何動作。
+        // 若未開啟檔案，則直接關閉表單。
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // 檢查是否開過檔案
+                if (!string.IsNullOrEmpty(OpenFile.FileName) && File.Exists(OpenFile.FileName))
+                {
+                    DialogResult result = MessageBox.Show("您確定要退出嗎？未保存的更改將會丟失。", "確認退出", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                    if (result == DialogResult.Yes)
+                    {
+                        // 關閉表單
+                        this.Close();
+                    }
+                }
+                else
+                {
+                    // 如果未開啟檔案，直接關閉表單
+                    this.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"發生錯誤: {ex.Message}", "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // label1_Click 事件目前未實作，保留作為未來擴充使用。
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // backgroundWorker3_DoWork 事件目前未實作，保留作為未來擴充使用。
+        private void backgroundWorker3_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+        }
+
+        // button_Click 事件用於新增一筆電話簿資料。
+        // 會從 textBox1 取得姓名，從 textBox2 取得電話號碼，並檢查是否為空。
+        // 若有開啟檔案，則可進行寫入（目前未完成寫入邏輯）。
+        private void button_Click(object sender, EventArgs e)
+        {
+            PhoneBookEntry entry; // 建立一個 PhoneBookEntry 結構物件
+            entry.name = textBox1.Text.Trim();   // 取得姓名並去除前後空白
+            entry.phone = textBox2.Text.Trim();  // 取得電話號碼並去除前後空白
+                                                 // 檢查姓名與電話號碼是否為空
+            if (!string.IsNullOrEmpty(OpenFile.FileName) && File.Exists(OpenFile.FileName))
+            {
+                StreamWriter writer =
+                    // 此處尚未完成寫入檔案的程式碼
+                }
+        }
+    }
+}
